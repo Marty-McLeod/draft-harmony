c2451fae147182cac62f872410c60c24de6c210f
Marty McLeod <martymcleod@yahoo.com>
2025-12-30 20:25:17 +0800

    Completed #26: new task & create, also marked outstanding issues as completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    @task
   end
 
   def new
     @task = Task.new
+    # Allow adding a Note in the simple_form, not requiring a separate form
+    @task.notes.build # Prepare one Note model field in the #new form
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
-    if @task.save
-      redirect_to tasks_path
+    if @task.save!
+      redirect_to task_path(@task), notice: "✔ Task and note created successfully"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 
   private
 
-  # def get_task
-  #   @task = Task.find(params[:id])
-  # end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
-    params.require(:task).permit(:title, :synopsis, :notes)
+    params.require(:task).permit(
+      :title, :synopsis,
+      notes_attributes: [:text]
+    )
   end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
+
   has_one :outline, dependent: :destroy
   has_many :notes, dependent: :destroy
 
+  # Allo nested attributes for notes
+  accepts_nested_attributes_for :notes, allow_destroy: true
+
+  # Mandates that at least a title and gen. prompt text must exist
+  validates :title, presence: true
+  validates :synopsis, presence: true
 end
