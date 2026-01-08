3742cf6e13bfc02b89022e3bd2e610dea1f35b7e
Marty McLeod <martymcleod@yahoo.com>
2026-01-08 18:54:23 +0900

    Completed issue #34: AI outline generation for tasks.

--- app/controllers/outlines_controller.rb
+++ app/controllers/outlines_controller.rb
@@ -1,6 +1,6 @@
 class OutlinesController < ApplicationController
 
-  SYSTEM_PROMPT_PREPEND =  "You are an English composition instructor. I am a college student with an essay writing assignment. Help me by outlining the"
+  SYSTEM_PROMPT_PREPEND =  "You are an English composition instructor. I am a college student with an essay writing assignment. "
   UNUSED = "main plot topics and characters in the book Moby Dick."
   SYSTEM_PROMPT_POSTEND = "Output format is Markdown only for the response."
   SYSTEM_PROMPT = "You are an English composition instructor. I am a college student with an essay writing assignment. Help me by outlining the main plot topics and characters in Moby Dick. Output format is Markdown only for the response."
@@ -34,8 +34,6 @@ class OutlinesController < ApplicationController
   end
 
   def generate
-    puts "#{params.inspect}"
-
     @task = Task.find(params[:task_id])
     @outline = Outline.find(params[:id])
     @outline.task = @task
@@ -44,10 +42,13 @@ class OutlinesController < ApplicationController
     #   @outline = Outline.new
     #   @outline.task = @task
     # end
-    # generate_system_prompt
-    # response = @chat.ask(SYSTEM_PROMPT)
-    # @outline.contents = response.contents
-    @outline.contents = "This is my\ntest code\nof three lines"
+    build_system_prompt
+    # prompt = "#{SYSTEM_PROMPT_PREPEND} #{@task.synopsis} #{SYSTEM_PROMPT_POSTEND}"
+    response = @chat.ask(@system_prompt)
+    # If it exists, strip out leading & trailing Markdown backticks which may appear from the ChatGPT chat model
+    # @outline.contents = response.content.gsub('```', '')
+    @outline.contents = response.content
+    
     if @outline.save
       redirect_to task_path(params[:task_id])
     else
@@ -55,11 +56,12 @@ class OutlinesController < ApplicationController
     end
   end
 
-  def generate_system_prompt
+  def build_system_prompt
     # Using user options, build the system prompt to be use for a chat agent response request (API)
-    user_directive = task.synopsis.text
+    user_directive = @task.synopsis
     # sanitize/strip user text as needed here!
-    @system_prompt = "#{SYSTEM_PROMPT_PREPEND} + #{user_directive} + #{SYSTEM_PROMPT_POSTEND}"
+    user_directive = user_directive.gsub("\n", "")
+    @system_prompt = prompt = "#{SYSTEM_PROMPT_PREPEND} #{user_directive} #{SYSTEM_PROMPT_POSTEND}"
   end
 
 
