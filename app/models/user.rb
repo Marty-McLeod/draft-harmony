d7e0824bc8c93fe645079b98c69eb3cf05c6e9cf
Marty McLeod <martymcleod@yahoo.com>
2026-02-01 22:03:17 +0800

    Completed #49, added user profile updates, included issue #48 done also.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,18 @@ class User < ApplicationRecord
   validate :acceptable_avatar
 
   def acceptable_avatar
-    unless avatar.byte_size <= 1.megabyte # Limit avatar image file size
-      errors.add(:avatar, "is too big; Image must be 1MB or less.")
-    end
+    # If an attachedment is present (file was uploaded in Devise #new or #edit) then do a file
+    # check to verify size & type for validation before creating/updating the user profile record
+    if self.avatar.attached?
+      unless avatar.byte_size <= 1.megabyte # Limit avatar image file size
+        errors.add(:avatar, "is too big; Image must be 1MB or less.")
+      end
 
-    acceptable_formats = ["image/jpg", "image/png", "image/wbp"]
+      acceptable_formats = ["image/jpg", "image/png", "image/wbp"]
 
-    unless acceptable_formats.include?(avatar.content_type)
-      errors.add(:avatar, "must be .JPG, .PNG, or .WEBP")
+      unless acceptable_formats.include?(avatar.content_type)
+        errors.add(:avatar, "must be .JPG, .PNG, or .WEBP")
+      end
     end
   end
 
